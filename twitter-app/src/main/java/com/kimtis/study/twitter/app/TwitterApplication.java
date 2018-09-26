aee551700d63d6460fa0bd139dff2c05c76fc92e
hsyou <realhs01@gmail.com>
2018-09-26 16:55:37 +0900

    jpa gradle - hibernate properties not found error

--- twitter-app/src/main/java/com/kimtis/study/twitter/app/TwitterApplication.java
+++ twitter-app/src/main/java/com/kimtis/study/twitter/app/TwitterApplication.java
@@ -2,7 +2,11 @@ package com.kimtis.study.twitter.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+@EntityScan(basePackages="com.kimtis.study.twitter.*")
+@EnableJpaRepositories(basePackages ="com.kimtis.study.twitter.*")
 @SpringBootApplication
 public class TwitterApplication {
 
